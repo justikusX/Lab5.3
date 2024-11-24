42729082ecbdcc49be14ce84a63d676a1655eec4
Екатерина Благочинная <e.blagochinnaya@dnevnik.ru>
2024-11-24 13:38:28 +0200

    Добавьте файлы проекта.

---
+++ Lab5.3/Program.cs
@@ -0,0 +1,6 @@
+﻿string originalString = "Привет, мир!";
+char[] charArray = originalString.ToCharArray();
+Array.Reverse(charArray);
+string reversedString = new string(charArray);
+
+Console.WriteLine(reversedString);
